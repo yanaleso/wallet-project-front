2fa9c51fe1ab04c986316ba51e076487557d5f4f
ZeinHome <zein3288@gmail.com>
2022-11-19 21:07:11 +0200

    LoginPage

--- src/pages/LoginPage/LoginPage.styled.jsx
+++ src/pages/LoginPage/LoginPage.styled.jsx
@@ -1,96 +1,102 @@
 import styled from 'styled-components';
+import EllipseRed from '../../images/EllipseRed.png';
+import EllipseViolet from '../../images/EllipseViolet.png';
 
 export const Section = styled.section`
-  position: relative;
-  padding: 107px 0px;
-  height: 100vh;
-  overflow: hidden;
-  z-index: -2;
-  @media screen and (min-width: 768px) and (max-width: 1279px) {
-    display: flex;
-    padding: 60px 0px 0px;
-    background-color: ${p => p.theme.colors.loginPageBg};
+  @media screen and (min-width: 768px) {
+    width: 100%;
+    min-height: 100vh;
+    background-color: #e7eaf2;
+    background-image: url(${EllipseViolet}), url(${EllipseRed});
+    background-position: 0 100%, 100% 0;
+    background-repeat: no-repeat;
   }
-  @media screen and (min-width: 1280px) {
-    display: flex;
-    padding: 0px;
-    background-color: ${p => p.theme.colors.loginPageBg};
-  }
+`;
+
+export const Blur = styled.div`
+  min-height: 100vh;
+  backdrop-filter: blur(50px);
 `;
 
 export const Container = styled.div`
-  max-width: 1420px;
-  margin: 0px auto;
-  padding: 0px 15px;
+  margin: 0 auto;
+  @media screen and (max-width: 767px) {
+    max-width: 480px;
+  }
+  @media screen and (min-width: 768px) {
+    width: 768px;
+  }
+  @media screen and (min-width: 1280px) {
+    width: 1280px;
+  }
 `;
 
-export const Frame = styled.img``;
-
-export const RedСircle = styled.div`
-  position: absolute;
-  right: -230px;
-  top: -170px;
-  width: 618px;
-  height: 547px;
-  border-radius: ${p => p.theme.radii.round};
-  background: #ffd8d0;
-  transform: rotate(21.94deg);
-  z-index: -1;
+export const Wrapper = styled.div`
+  display: flex;
+  @media screen and (max-width: 767px) {
+    align-items: center;
+    height: 100vh;
+    max-width: 480px;
+  }
+  @media screen and (min-width: 768px) and (max-width: 1279px) {
+    flex-direction: column;
+    padding: 38px 0;
+    max-width: none;
+    height: 100%;
+  }
+  @media screen and (min-width: 1280px) {
+    flex-direction: row;
+    height: 100%;
+    max-width: none;
+    align-items: center;
+    padding: 0;
+  }
 `;
 
-export const RedСircleText = styled.p`
-  position: absolute;
-  left: 70px;
-  bottom: 100px;
-  font-family: 'Poppins';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 30px;
-  line-height: 45px;
-  transform: rotate(-22deg);
-  color: ${p => p.theme.colors.primaryText};
+export const Frame = styled.img`
+  @media screen and (min-width: 768px) and (max-width: 1279px) {
+    height: 250px;
+    width: 250px;
+    margin-right: 40px;
+  }
+  @media screen and (min-width: 1280px) {
+    margin-bottom: 32px;
+  }
 `;
 
-export const VioletСircle = styled.div`
-  position: absolute;
-  left: -55px;
-  bottom: -150px;
-  width: 618px;
-  height: 547px;
-  border-top-left-radius: 115%;
-  border-top-right-radius: 60%;
-  border-bottom-right-radius: 10%;
-  background: #c5baff;
-  transform: rotate(36deg);
-  z-index: -1;
+export const ImageContainer = styled.div`
+  @media screen and (min-width: 768px) and (max-width: 1279px) {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    margin-bottom: 50px;
+  }
+  @media screen and (min-width: 1280px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-right: 66px;
+  }
 `;
-
-export const VioletСircleText = styled.p`
-  position: fixed;
-  right: 180px;
-  top: 190px;
+export const Text = styled.p`
   font-family: 'Poppins';
   font-style: normal;
   font-weight: 400;
   font-size: 30px;
   line-height: 1.5;
-  transform: rotate(-36deg);
+
   color: ${p => p.theme.colors.primaryText};
 `;
-
-export const ImageContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  width: 50vw;
-`;
 export const FormContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  width: 50vw;
-  /* backdrop-filter: blur(25px); */
-  background-color: rgba(255, 255, 255, 0.4);
+  @media screen and (min-width: 1280px) {
+    width: auto;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    backdrop-filter: blur(50px);
+    background-color: rgba(255, 255, 255, 0.4);
+    padding: 52px 91px 52px 107px;
+  }
 `;

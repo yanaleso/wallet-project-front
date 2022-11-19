14c39ba5631e08a488381feee53f715f9aa8328b
Preksler <preksler@gmail.com>
2022-11-20 01:36:15 +0200

    DashboardPage

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -2,22 +2,73 @@ import SharedLayout from 'components/SharedLayout';
 import Currency from 'components/Currency';
 import Header from 'components/Header';
 import Navigation from 'components/Navigation';
-import { Suspense } from 'react';
+import Media from 'react-media';
+import { Suspense, Fragment } from 'react';
 import { Outlet } from 'react-router-dom';
+import { Section, Blur, Container, Wrapper } from './Dashboard.styled';
 
 const DashboardPage = () => {
   return (
     <SharedLayout>
       <Header />
-      <main style={{ display: 'flex', alignItems: "flex-end", justifyContent: 'space-evenly' }}>
-        <div>
-          <Navigation />
-          <h2>Balanse 24 000</h2>
-          <Currency />
-        </div>
-        <Suspense fallback={null}>
-          <Outlet />
-        </Suspense>
+      <main>
+        <Section>
+          <Media
+            queries={{
+              small: '(max-width: 767px)',
+              medium: '(min-width: 768px) and (max-width: 1279px)',
+              large: '(min-width: 1280px)',
+            }}
+          >
+            {matches => (
+              <Fragment>
+                {matches.small && (
+                  <Container>
+                    <Wrapper>
+                      <Navigation />
+                      <h2>Balanse 24 000</h2>
+                      <Suspense fallback={null}>
+                        <Outlet />
+                      </Suspense>
+                    </Wrapper>
+                  </Container>
+                )}
+                {matches.medium && (
+                  <Blur>
+                    <Container>
+                      <Wrapper>
+                        <div>
+                          <Navigation />
+                          <h2>Balanse 24 000</h2>
+                        </div>
+                        <Currency />
+                      </Wrapper>
+                      <Suspense fallback={null}>
+                        <Outlet />
+                      </Suspense>
+                    </Container>
+                  </Blur>
+                )}
+                {matches.large && (
+                  <Blur>
+                    <Container>
+                      <Wrapper>
+                        <div>
+                          <Navigation />
+                          <h2>Balanse 24 000</h2>
+                          <Currency />
+                        </div>
+                        <Suspense fallback={null}>
+                          <Outlet />
+                        </Suspense>
+                      </Wrapper>
+                    </Container>
+                  </Blur>
+                )}
+              </Fragment>
+            )}
+          </Media>
+        </Section>
       </main>
     </SharedLayout>
   );

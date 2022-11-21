cf90bd5e84c627e6beefaf7e74f9718a798dd7dc
Krasnonos <andreykrasnonos@icloud.com>
2022-11-21 13:14:40 -0500

    add authOperationLogic

---
+++ src/redux/auth/authOperation.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://wallet-project.onrender.com/api';
+
+const token = {
+  set(token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common['Authorization'] = ``;
+  },
+};
+
+export const userRegistration = createAsyncThunk(
+  'users/signup',
+  async (userData, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post('/users/signup', userData);
+      token.set(data.token);
+      console.log(data);
+      return data;
+    } catch (error) {
+      return rejectWithValue('Registration has failed');
+    }
+  }
+);
+
+export const userLogin = createAsyncThunk(
+  'users/login',
+  async (userData, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post('/users/signup', userData);
+      token.set(data.token);
+      console.log(data);
+      return data;
+    } catch (error) {
+      return rejectWithValue('login has failed');
+    }
+  }
+);
+
+export const userLogout = createAsyncThunk(
+  'users/logout',
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get('/users/signup');
+      token.unset();
+      console.log(data);
+      return data;
+    } catch (error) {
+      return rejectWithValue('logout has failed');
+    }
+  }
+);
--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,16 +1,44 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { userRegistration, userLogin, userLogout } from './authOperation';
 
 const initialState = {
   user: { name: null, email: null },
   token: null,
-  isLoggedIn: true,
+  isLoggedIn: false,
   isRefreshingUser: false,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {},
+  extraReducers: {
+    [userRegistration.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+      state.error = null;
+    },
+    [userRegistration.rejected](state, action) {
+      state.error = action.payload;
+    },
+    [userLogin.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+      state.error = null;
+    },
+    [userLogin.rejected](state, action) {
+      state.error = action.payload;
+    },
+    [userLogout.fulfilled](state) {
+      state.user = { name: null, email: null };
+      state.token = null;
+      state.isLoggedIn = false;
+    },
+    [userLogout.rejected](state, action) {
+      state.error = action.payload;
+    },
+  },
 });
 
 export default authSlice.reducer;

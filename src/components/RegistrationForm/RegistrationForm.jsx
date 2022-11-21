ba26dfe2d87e693530da81b70f0e421cde7c93ea
Krasnonos <andreykrasnonos@icloud.com>
2022-11-21 08:46:33 -0500

    add validation

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,17 +1,53 @@
 import { Link } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+import shema from 'helpers';
 import { StyledForm } from './RegistrationForm.styled';
 
+const initialValues = {
+  email: '',
+  password: '',
+  confirmPassword: '',
+  name: '',
+};
+
 const RegisterForm = () => {
+  const handleSubmit = (values, { resetForm }) => {
+    console.log(values);
+  };
+
   return (
     <div>
-      <StyledForm>
-        <input type="emuil" />
-        <input type="password" />
-        <input type="password" />
-        <input type="text" />
-        <button type="submit">Register</button>
-        <Link to="/login">Login</Link>
-      </StyledForm>
+      <h1>LOGO</h1>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={shema.register}
+      >
+        <Form>
+          <label>
+            Em:
+            <Field type="email" name="email" placeholder="E-mail" />
+          </label>
+          <label>
+            Pas:
+            <Field type="password" name="password" placeholder="Password" />
+          </label>
+          <label>
+            CP:
+            <Field
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirm password"
+            />
+          </label>
+          <label>
+            Ne:
+            <Field type="text" name="name" placeholder="First name" />
+          </label>
+          <button type="submit">register</button>
+        </Form>
+      </Formik>
+      <Link to="/login">log in</Link>
     </div>
   );
 };

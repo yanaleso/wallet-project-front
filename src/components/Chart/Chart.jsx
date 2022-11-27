f2bf226552ebbab4eb0ec190c09ba5cd15f1a969
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-11-28 00:41:47 +0200

    fix chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -8,32 +8,33 @@ import {
 import { Doughnut } from 'react-chartjs-2';
 import { selectAllStatistic } from 'redux/statistic/statisticSelectors';
 import { useSelector } from 'react-redux';
+//import { categoriesColors } from '../../helpers/categoriesColors';
 
 ChartJS.register(ArcElement, Tooltip, Legend, DoughnutController);
 
 const Chart = () => {
   const res = useSelector(selectAllStatistic);
-  const results = res.statistic;
+  const expenseResults = res.statistic.filter(
+    result => result.type === 'expense'
+  );
 
-  const chartCategories = results.map(result => result._id);
-  const sum = results.map(result => result.totalSum);
+  const { totalBalance } = useSelector(state => state.transactions);
+  const chartCategories = expenseResults.map(result => result._id);
+  const sum = expenseResults.map(result => result.totalSum);
 
   const drawInnerText = chart => {
     const ctx = chart.ctx;
     ctx.restore();
     ctx.font = '700 18px Circe-Regular';
     ctx.textBaseline = 'middle';
-    const dataArrValues = chart.config._config.data.datasets[0].data;
-    ctx.cutoutPercentage = 30;
-    const text = `₴${dataArrValues
-      .reduce((total, b) => total + b, 0)
-      .toFixed(2)}`;
+    const text = `₴${totalBalance}`;
     const textX = Math.round((chart.width - ctx.measureText(text).width) / 2);
     const textY = chart.height / 2 + chart.legend.height / 2;
     ctx.fillText(text, textX, textY);
     ctx.fillStyle = '#000000';
     ctx.save();
   };
+
   const data = {
     labels: chartCategories,
     datasets: [
@@ -44,7 +45,6 @@ const Chart = () => {
         backgroundColor: [
           '#FED057',
           '#FFD8D0',
-
           '#C5BAFF',
           '#6E78E8',
           '#81E1FF',

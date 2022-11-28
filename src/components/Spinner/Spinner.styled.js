54fc5d706f87c9110ad6eea8c057067e1d3efad2
Anna Leszka <ania.lalala97@gmail.com>
2022-11-28 18:44:07 +0100

    fix loader

--- src/components/Spinner/Spinner.styled.js
+++ src/components/Spinner/Spinner.styled.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
-import { RotatingLines } from 'react-loader-spinner';
 
-export const Loader = styled(RotatingLines)`
+export const SpinnerBox =  styled.div`
     position: fixed;
     top: 50%;
     left: 50%;

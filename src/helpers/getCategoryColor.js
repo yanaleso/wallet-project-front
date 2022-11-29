fdf391ec1c8c26db754e2c65fb6183f6720d854e
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-11-29 10:41:23 +0200

    Update getCategoryColor.js

--- src/helpers/getCategoryColor.js
+++ src/helpers/getCategoryColor.js
@@ -1,12 +1,12 @@
 const categoryList = {
-'Food':           '#FED057',
-'Main':           '#FFD8D0',
-'House':          '#C5BAFF',
-'Auto':           '#6E78E8',
-'Children':       '#81E1FF',
-'Development':    '#FD9498',
-'Education':      '#C5BAFF',
-'Other expenses': '#FD9498',
-}
-  
-export const getCategoryColor = (value) => categoryList[value]
+  Food: '#FED057',
+  Main: '#FFD8D0',
+  House: '#C5BAFF',
+  Auto: '#6E78E8',
+  Children: '#81E1FF',
+  Development: '#FD9498',
+  Education: '#C5BAFF',
+  'Other expenses': '#00AD84',
+};
+
+export const getCategoryColor = value => categoryList[value];

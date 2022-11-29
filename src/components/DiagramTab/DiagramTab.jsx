72d66e74bf68c6a52b1f87b747759296aa4082cb
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-11-29 02:01:47 +0200

    add styles for responsive

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -1,10 +1,6 @@
-// import { DiagramTabItem } from './DiagramTabItem';
+import { useMedia } from 'react-use';
 import Chart from '../Chart';
 import Table from '../Table';
-// import { Formik } from 'formik';
-// import Select from 'react-select';
-// import { months } from 'helpers/monthList';
-// import { years } from 'helpers/yearList';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectAllStatistic } from 'redux/statistic/statisticSelectors';
 import { useEffect } from 'react';
@@ -14,10 +10,12 @@ import {
   StyledTitle,
   StyledContentWraper,
 } from './DiagramTab.styled';
-//import { getCategoryColor } from 'helpers/getCategoryColor';
+
 import { getStatistic } from 'redux/statistic/statisticOperation';
 
 const DiagramTab = () => {
+  const isMobie = useMedia('(max-width: 767px)');
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -27,163 +25,33 @@ const DiagramTab = () => {
   const res = useSelector(selectAllStatistic);
   const data = res.statistic;
   console.log('data', data);
-  //const screenWidth = window.screen.width;
 
   return (
-    <StyledStatisticWraper>
-      <StyledTitle>Statistics</StyledTitle>
-      <StyledContentWraper>
-        <StyledChartWraper>
-          <Chart />
-        </StyledChartWraper>
+    <div>
+      {isMobie ? (
+        <StyledStatisticWraper>
+          <StyledTitle>Statistics</StyledTitle>
 
-        <Table />
-      </StyledContentWraper>
-    </StyledStatisticWraper>
+          <StyledChartWraper>
+            <Chart />
+          </StyledChartWraper>
+
+          <Table />
+        </StyledStatisticWraper>
+      ) : (
+        <StyledStatisticWraper>
+          <StyledTitle>Statistics</StyledTitle>
+          <StyledContentWraper>
+            <StyledChartWraper>
+              <Chart />
+            </StyledChartWraper>
+
+            <Table />
+          </StyledContentWraper>
+        </StyledStatisticWraper>
+      )}
+    </div>
   );
-  //   if (screenWidth >= 768) {
-  //     return (
-  //       <>
-  //         {/* {isLoading && <b>Loading tasks...</b>}
-  //         {error && <b>{error}</b>} */}
-  //         <StyledTable>
-  //           <Formik>
-  //             <StyledFilters>
-  //               <Select
-  //                 name="month"
-  //                 options={months}
-  //                 isClearable
-  //                 isSearchable
-  //                 placeholder={<div>Month</div>}
-  //                 styles={{
-  //                   control: (baseStyles, state) => ({
-  //                     ...baseStyles,
-  //                     borderColor: state.isFocused
-  //                       ? 'hsl(0, 0%, 70%)'
-  //                       : '#000000',
-  //                     borderRadius: '30px',
-  //                     width: '181px',
-  //                     height: '50px',
-  //                   }),
-  //                   indicatorSeparator: (baseStyles, state) => ({
-  //                     ...baseStyles,
-  //                     display: 'none',
-  //                   }),
-  //                   placeholder: (baseStyles, state) => ({
-  //                     position: 'absolute',
-  //                     marginLeft: '20px',
-  //                     fontFamily: 'Circe-Regular',
-  //                     fontWeight: '400',
-  //                     fontSize: '16px',
-  //                     lineHeight: '24px',
-  //                     color: '#000000',
-  //                   }),
-  //                   Svg: (baseStyles, state) => ({
-  //                     ...baseStyles,
-  //                     fill: '#000000',
-  //                   }),
-  //                 }}
-  //               />
-  //               <Select
-  //                 name="year"
-  //                 options={years}
-  //                 isClearable
-  //                 isSearchable
-  //                 placeholder={<div>Year</div>}
-  //                 styles={{
-  //                   control: (baseStyles, state) => ({
-  //                     ...baseStyles,
-  //                     borderColor: state.isFocused
-  //                       ? 'hsl(0, 0%, 70%)'
-  //                       : '#000000',
-  //                     borderRadius: '30px',
-  //                     width: '181px',
-  //                     height: '50px',
-  //                   }),
-  //                   indicatorSeparator: (baseStyles, state) => ({
-  //                     ...baseStyles,
-  //                     display: 'none',
-  //                   }),
-  //                   placeholder: (baseStyles, state) => ({
-  //                     position: 'absolute',
-  //                     marginLeft: '20px',
-  //                     fontFamily: 'Circe-Regular',
-  //                     fontWeight: '400',
-  //                     fontSize: '16px',
-  //                     lineHeight: '24px',
-  //                     color: '#000000',
-  //                   }),
-  //                   Svg: (baseStyles, state) => ({
-  //                     ...baseStyles,
-  //                     fill: '#000000',
-  //                   }),
-  //                 }}
-  //               />
-  //             </StyledFilters>
-  //           </Formik>
-  //           <StyledTableHeader>
-  //             <p>Category</p>
-  //             <p>Sum</p>
-  //           </StyledTableHeader>
-
-  //           <StyledTableBody>
-  //             <ul>
-  //               {data.map(({ _id, type, totalSum }) => {
-  //                 if (type === 'expense') {
-  //                   return (
-  //                     <li key={_id}>
-  //                       <StyledItem>
-  //                         <StyledInnerSpan
-  //                           category={getCategoryColor(_id)}
-  //                           style={{
-  //                             width: '24px',
-  //                             height: '24px',
-  //                             marginRight: '10px',
-  //                           }}
-  //                         ></StyledInnerSpan>
-  //                         <p>{_id}</p>
-  //                       </StyledItem>
-  //                       <p>
-  //                         <span
-  //                           style={{
-  //                             color: type === 'income' ? '#24CCA7' : '#FF6596',
-  //                           }}
-  //                         >
-  //                           {totalSum}
-  //                         </span>
-  //                       </p>
-  //                     </li>
-  //                   );
-  //                 }
-  //                 return null
-  //               })
-  //               }
-  //             </ul>
-  //           </StyledTableBody>
-  //           <StyledTableFooter>
-  //             <li>
-  //               <p>Expenses:</p>
-  //               <p style={{ color: '#FF6596' }}>{expensesTotal}</p>
-  //             </li>
-  //             <li>
-  //               <p>Income:</p>
-  //               <p style={{ color: '#24CCA7' }}>{incomeTotal}</p>
-  //             </li>
-  //           </StyledTableFooter>
-  //         </StyledTable>
-  //       </>
-  //     );
-  //   } else {
-  //     return (
-  //       <StyledWrap>
-  //         {data.map(({ _id, type, totalSum, balance }) => (
-  //           <DiagramTabItem
-  //             //key={id}
-  //             statistic={{ _id, type, totalSum, balance }}
-  //           />
-  //         ))}
-  //       </StyledWrap>
-  //     );
 };
 
 export default DiagramTab;
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -7,16 +7,21 @@ import {
   StyledItem,
   StyledInnerSpan,
 } from './Table.styled';
+import { useMedia } from 'react-use';
 import { Formik } from 'formik';
 import Select from 'react-select';
 import { months } from 'helpers/monthList';
 import { years } from 'helpers/yearList';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectAllStatistic } from 'redux/statistic/statisticSelectors';
+
 // import { useEffect } from 'react';
 import { getCategoryColor } from 'helpers/getCategoryColor';
 // import { getStatistic } from 'redux/statistic/statisticOperation'
+
 const Table = ({ _id, type, totalSum }) => {
+  const isMobie = useMedia('(max-width: 767px)');
+
   const res = useSelector(selectAllStatistic);
   const data = res.statistic;
   const incomeTotal = data
@@ -31,126 +36,269 @@ const Table = ({ _id, type, totalSum }) => {
     <>
       {/* {isLoading && <b>Loading tasks...</b>}
         {error && <b>{error}</b>} */}
-      <StyledTable>
-        <Formik>
-          <StyledFilters>
-            <Select
-              name="month"
-              options={months}
-              isClearable
-              isSearchable
-              placeholder={<div>Month</div>}
-              styles={{
-                control: (baseStyles, state) => ({
-                  ...baseStyles,
-                  borderColor: state.isFocused ? 'hsl(0, 0%, 70%)' : '#000000',
-                  borderRadius: '30px',
-                  width: '181px',
-                  height: '50px',
-                }),
-                indicatorSeparator: (baseStyles, state) => ({
-                  ...baseStyles,
-                  display: 'none',
-                }),
-                placeholder: (baseStyles, state) => ({
-                  position: 'absolute',
-                  marginLeft: '20px',
-                  fontFamily: 'Circe-Regular',
-                  fontWeight: '400',
-                  fontSize: '16px',
-                  lineHeight: '24px',
-                  color: '#000000',
-                }),
-                Svg: (baseStyles, state) => ({
-                  ...baseStyles,
-                  fill: '#000000',
-                }),
-              }}
-            />
-            <Select
-              name="year"
-              options={years}
-              isClearable
-              isSearchable
-              placeholder={<div>Year</div>}
-              styles={{
-                control: (baseStyles, state) => ({
-                  ...baseStyles,
-                  borderColor: state.isFocused ? 'hsl(0, 0%, 70%)' : '#000000',
-                  borderRadius: '30px',
-                  width: '181px',
-                  height: '50px',
-                }),
-                indicatorSeparator: (baseStyles, state) => ({
-                  ...baseStyles,
-                  display: 'none',
-                }),
-                placeholder: (baseStyles, state) => ({
-                  position: 'absolute',
-                  marginLeft: '20px',
-                  fontFamily: 'Circe-Regular',
-                  fontWeight: '400',
-                  fontSize: '16px',
-                  lineHeight: '24px',
-                  color: '#000000',
-                }),
-                Svg: (baseStyles, state) => ({
-                  ...baseStyles,
-                  fill: '#000000',
-                }),
-              }}
-            />
-          </StyledFilters>
-        </Formik>
-        <StyledTableHeader>
-          <p>Category</p>
-          <p>Sum</p>
-        </StyledTableHeader>
+      {isMobie ? (
+        <StyledTable>
+          <Formik>
+            <StyledFilters>
+              <Select
+                name="month"
+                options={months}
+                isClearable
+                isSearchable
+                placeholder={<div>Month</div>}
+                styles={{
+                  control: (baseStyles, state) => ({
+                    ...baseStyles,
+                    borderColor: state.isFocused
+                      ? 'hsl(0, 0%, 70%)'
+                      : '#000000',
+                    borderRadius: '30px',
+                    marginBottom: '20px',
+                    marginTop: '0px',
+                    marginRight: 'auto',
+                    marginLeft: 'auto',
+                    width: '280px',
+                    height: '50px',
+                    backgroundColor: 'transparent',
+                  }),
+                  indicatorSeparator: (baseStyles, state) => ({
+                    ...baseStyles,
+                    display: 'none',
+                  }),
+                  placeholder: (baseStyles, state) => ({
+                    position: 'absolute',
+                    marginLeft: '20px',
+                    fontFamily: 'Circe-Regular',
+                    fontWeight: '400',
+                    fontSize: '16px',
+                    lineHeight: '24px',
+                    color: '#000000',
+                  }),
+                  Svg: (baseStyles, state) => ({
+                    ...baseStyles,
+                    fill: '#000000',
+                  }),
+                }}
+              />
+              <Select
+                name="year"
+                options={years}
+                isClearable
+                isSearchable
+                placeholder={<div>Year</div>}
+                styles={{
+                  control: (baseStyles, state) => ({
+                    ...baseStyles,
+                    borderColor: state.isFocused
+                      ? 'hsl(0, 0%, 70%)'
+                      : '#000000',
+                    borderRadius: '30px',
+                    marginBottom: '20px',
+                    marginTop: '0px',
+                    marginRight: 'auto',
+                    marginLeft: 'auto',
+                    width: '280px',
+                    height: '50px',
+                    backgroundColor: 'transparent',
+                  }),
+                  indicatorSeparator: (baseStyles, state) => ({
+                    ...baseStyles,
+                    display: 'none',
+                  }),
+                  placeholder: (baseStyles, state) => ({
+                    position: 'absolute',
+                    marginLeft: '20px',
+                    fontFamily: 'Circe-Regular',
+                    fontWeight: '400',
+                    fontSize: '16px',
+                    lineHeight: '24px',
+                    color: '#000000',
+                  }),
+                  Svg: (baseStyles, state) => ({
+                    ...baseStyles,
+                    fill: '#000000',
+                  }),
+                }}
+              />
+            </StyledFilters>
+          </Formik>
+          <StyledTableHeader>
+            <p>Category</p>
+            <p>Sum</p>
+          </StyledTableHeader>
 
-        <StyledTableBody>
-          <ul>
-            {data.map(({ _id, type, totalSum }) => {
-              if (type === 'expense') {
-                return (
-                  <li key={_id}>
-                    <StyledItem>
-                      <StyledInnerSpan
-                        category={getCategoryColor(_id)}
-                        style={{
-                          width: '24px',
-                          height: '24px',
-                          marginRight: '10px',
-                        }}
-                      ></StyledInnerSpan>
-                      <p>{_id}</p>
-                    </StyledItem>
-                    <p>
-                      <span
-                        style={{
-                          color: type === 'income' ? '#24CCA7' : '#FF6596',
-                        }}
-                      >
-                        {totalSum}
-                      </span>
-                    </p>
-                  </li>
-                );
-              }
-              return null;
-            })}
-          </ul>
-        </StyledTableBody>
-        <StyledTableFooter>
-          <li>
-            <p>Expenses:</p>
-            <p style={{ color: '#FF6596' }}>{expensesTotal}</p>
-          </li>
-          <li>
-            <p>Income:</p>
-            <p style={{ color: '#24CCA7' }}>{incomeTotal}</p>
-          </li>
-        </StyledTableFooter>
-      </StyledTable>
+          <StyledTableBody>
+            <ul>
+              {data.map(({ _id, type, totalSum }) => {
+                if (type === 'expense') {
+                  return (
+                    <li key={_id}>
+                      <StyledItem>
+                        <StyledInnerSpan
+                          category={getCategoryColor(_id)}
+                          style={{
+                            width: '24px',
+                            height: '24px',
+                            marginRight: '10px',
+                          }}
+                        ></StyledInnerSpan>
+                        <p>{_id}</p>
+                      </StyledItem>
+                      <p>
+                        <span
+                          style={{
+                            color: type === 'income' ? '#24CCA7' : '#FF6596',
+                          }}
+                        >
+                          {totalSum}
+                        </span>
+                      </p>
+                    </li>
+                  );
+                }
+                return null;
+              })}
+            </ul>
+          </StyledTableBody>
+          <StyledTableFooter>
+            <li>
+              <p>Expenses:</p>
+              <p style={{ color: '#FF6596' }}>{expensesTotal}</p>
+            </li>
+            <li>
+              <p>Income:</p>
+              <p style={{ color: '#24CCA7' }}>{incomeTotal}</p>
+            </li>
+          </StyledTableFooter>
+        </StyledTable>
+      ) : (
+        <StyledTable>
+          <Formik>
+            <StyledFilters>
+              <Select
+                name="month"
+                options={months}
+                isClearable
+                isSearchable
+                placeholder={<div>Month</div>}
+                styles={{
+                  control: (baseStyles, state) => ({
+                    ...baseStyles,
+                    borderColor: state.isFocused
+                      ? 'hsl(0, 0%, 70%)'
+                      : '#000000',
+                    borderRadius: '30px',
+                    width: '181px',
+                    height: '50px',
+                    backgroundColor: 'transparent',
+                  }),
+                  indicatorSeparator: (baseStyles, state) => ({
+                    ...baseStyles,
+                    display: 'none',
+                  }),
+                  placeholder: (baseStyles, state) => ({
+                    position: 'absolute',
+                    marginLeft: '20px',
+                    fontFamily: 'Circe-Regular',
+                    fontWeight: '400',
+                    fontSize: '16px',
+                    lineHeight: '24px',
+                    color: '#000000',
+                  }),
+                  Svg: (baseStyles, state) => ({
+                    ...baseStyles,
+                    fill: '#000000',
+                  }),
+                }}
+              />
+              <Select
+                name="year"
+                options={years}
+                isClearable
+                isSearchable
+                placeholder={<div>Year</div>}
+                styles={{
+                  control: (baseStyles, state) => ({
+                    ...baseStyles,
+                    borderColor: state.isFocused
+                      ? 'hsl(0, 0%, 70%)'
+                      : '#000000',
+                    borderRadius: '30px',
+                    width: '181px',
+                    height: '50px',
+                    backgroundColor: 'transparent',
+                  }),
+                  indicatorSeparator: (baseStyles, state) => ({
+                    ...baseStyles,
+                    display: 'none',
+                  }),
+                  placeholder: (baseStyles, state) => ({
+                    position: 'absolute',
+                    marginLeft: '20px',
+                    fontFamily: 'Circe-Regular',
+                    fontWeight: '400',
+                    fontSize: '16px',
+                    lineHeight: '24px',
+                    color: '#000000',
+                  }),
+                  Svg: (baseStyles, state) => ({
+                    ...baseStyles,
+                    fill: '#000000',
+                  }),
+                }}
+              />
+            </StyledFilters>
+          </Formik>
+          <StyledTableHeader>
+            <p>Category</p>
+            <p>Sum</p>
+          </StyledTableHeader>
+
+          <StyledTableBody>
+            <ul>
+              {data.map(({ _id, type, totalSum }) => {
+                if (type === 'expense') {
+                  return (
+                    <li key={_id}>
+                      <StyledItem>
+                        <StyledInnerSpan
+                          category={getCategoryColor(_id)}
+                          style={{
+                            width: '24px',
+                            height: '24px',
+                            marginRight: '10px',
+                          }}
+                        ></StyledInnerSpan>
+                        <p>{_id}</p>
+                      </StyledItem>
+                      <p>
+                        <span
+                          style={{
+                            color: type === 'income' ? '#24CCA7' : '#FF6596',
+                          }}
+                        >
+                          {totalSum}
+                        </span>
+                      </p>
+                    </li>
+                  );
+                }
+                return null;
+              })}
+            </ul>
+          </StyledTableBody>
+          <StyledTableFooter>
+            <li>
+              <p>Expenses:</p>
+              <p style={{ color: '#FF6596' }}>{expensesTotal}</p>
+            </li>
+            <li>
+              <p>Income:</p>
+              <p style={{ color: '#24CCA7' }}>{incomeTotal}</p>
+            </li>
+          </StyledTableFooter>
+        </StyledTable>
+      )}
     </>
   );
 };
